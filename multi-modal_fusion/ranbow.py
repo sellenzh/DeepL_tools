c96d59eeef209b635842a96c9011ebc08b46f9a5
sellen <sellenzh@gmail.com>
2023-06-04 21:43:07 +0800

    main

--- multi-modal_fusion/ranbow.py
+++ multi-modal_fusion/ranbow.py
@@ -1,3 +1,10 @@
 import torch
 from torch import nn
 
+class RanbowFusion(nn.Module):
+    def __init__(self):
+        super(RanbowFusion, self).__init__()
+        
+
+    def forward(self, x):
+        pass
